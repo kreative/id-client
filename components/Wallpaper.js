10dc04d31e78aa2b122523456d349a3a17343d0d
Armaan Gupta <armaan@guppy.im>
2023-01-17 08:31:41 -0600

    feat: created wallpaper component

---
+++ components/Wallpaper.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function WallpaperComponent() {
+  return (
+    <>
+      <img
+        className="absolute inset-0 h-full w-full object-cover"
+        src="/space.jpg"
+        alt="Space image Wallpaper for Kreative ID"
+      />
+    </>
+  );
+}
