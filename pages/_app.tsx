696809bfbddbcf8cfb2eff650c7e8776710724c2
Armaan Gupta <armaan@guppy.im>
2023-01-30 11:38:42 -0600

    fix: changed html to dev and removed inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CookiesProvider } from "react-cookie";
-import { Inter } from '@next/font/google';
 import localFont from "@next/font/local";
 
-// we still need the inter font for initial UI hydration
-const inter = Inter({ subsets: ['latin'] });
 // custom craftwork sans fonts loading locally
 const craftworkSans = localFont({
   src: [
@@ -46,9 +43,9 @@ const craftworkSans = localFont({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CookiesProvider>
-      <html className={`${craftworkSans.variable} font-sans`}>
+      <div className={`${craftworkSans.variable} font-sans`}>
         <Component {...pageProps} />
-      </html>
+      </div>
     </CookiesProvider>
   );
 }
