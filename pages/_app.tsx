c766779ffb5e456527a1c3cd984f2400da4bf2cc
Armaan Gupta <armaan@guppy.im>
2023-01-30 11:19:40 -0600

    feat: added custom CraftworkSans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,51 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CookiesProvider } from "react-cookie";
 import { Inter } from '@next/font/google';
+import localFont from "@next/font/local";
 
 const inter = Inter({ subsets: ['latin'] });
+const craftworkSans = localFont({
+  src: [
+    {
+      path: '../styles/fonts/CraftworkSans-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkSans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkSans-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkSans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../styles/fonts/CraftworkSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-craftworksans',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CookiesProvider>
-      <Component {...pageProps} />
+      <html className={`${craftworkSans.variable} font-sans`}>
+        <Component {...pageProps} />
+      </html>
     </CookiesProvider>
   );
 }
