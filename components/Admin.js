588bbd1405d9b28194ae305879dfd9c45ed5896f
Armaan Gupta <armaan@guppy.im>
2023-01-30 20:05:54 -0600

    feat: created logout button functionality

--- components/Admin.js
+++ components/Admin.js
@@ -4,6 +4,77 @@ import axios from "axios";
 import { useCookies } from "react-cookie";
 
 function AdminFunction({ apps }) {
+  const [cookies, setCookie, removeCookie] = useCookies([
+    "kreative_id_key",
+    "keychain_id",
+    "id_ksn",
+    "id_email",
+    "id_fname",
+    "id_lname",
+    "id_picture",
+  ]);
+
+  const logout = (e) => {
+    // prevents default button click behavior
+    e.preventDefault();
+
+    // closes the keychain using id-api
+    axios
+      .post(
+        `https://id-api.kreativeusa.com/v1/keychains/${cookies["keychain_id"]}/close`
+      )
+      .then((response) => {
+        // response status code is between 200-299
+        // the only response that would come through is 200 (HTTP OK)
+        console.log(response.data.data);
+
+        // deletes all cookies stored in local storage
+        removeCookie("kreative_id_key");
+        removeCookie("keychain_id");
+        removeCookie("id_ksn");
+        removeCookie("id_email");
+        removeCookie("id_fname");
+        removeCookie("id_lname");
+        removeCookie("id_picture");
+
+        // redirects the user to the logout confirmation page
+        window.location.href = "/logged-out";
+      })
+      .catch((error) => {
+        // error response status code is above 300+
+        console.log(error);
+
+        // gets the status code of the error through the response
+        // we check to see if the status code exists as to not throw an error in case an error is thrown
+        // that isn't actually sent by the server api, but rather from axios
+        let statusCode;
+
+        if (error.response.data.data.statusCode === undefined) {
+          statusCode = error.response.data.data.statusCode;
+
+          // for all of these errors we want to redirect the user to the error page with a cause
+          if (statusCode === 403) {
+            // bad request, probably the id was not passed or is not a number
+            window.location.href = "/error?cause=badrequest";
+          } else if (statusCode === 404) {
+            // keychain not found using the id
+            window.location.href = "/error?cause=notfound";
+          } else if (statusCode === 500) {
+            // internal server error
+            window.location.href = "/error?cause=ise";
+          } else {
+            // some weird unknown error
+            // this should not happen at all, so if it does there are critical issues
+            window.location.href = "/error?cause=unknown";
+          }
+        } else {
+          // if there is no error response status code then we have an "unknown error"
+          // in most cases this is a connection error or some sort of axios error
+          window.location.href = "/error?cause=unknown";
+        }
+      });
+  };
+
   return (
     <div className="mx-auto max-w-7xl px-4 pt-8 sm:px-6 lg:px-8">
       <div className="md:flex md:items-center md:justify-between">
@@ -13,13 +84,14 @@ function AdminFunction({ apps }) {
           </h2>
         </div>
         <div className="mt-4 flex md:mt-0 md:ml-4">
-        <button
-          type="button"
-          className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          Logout
-        </button>
-      </div>
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            onClick={(e) => logout(e)}
+          >
+            Logout
+          </button>
+        </div>
       </div>
       <div className="pt-12">
         <div className="sm:flex sm:items-center">
@@ -125,9 +197,9 @@ export default function AdminComponent() {
       axios
         .get("https://id-api.kreativeusa.com/v1/applications", {
           headers: {
-            "KREATIVE_ID_KEY": cookies["kreative_id_key"],
-            "KREATIVE_AIDN": process.env.NEXT_PUBLIC_AIDN,
-          }
+            KREATIVE_ID_KEY: cookies["kreative_id_key"],
+            KREATIVE_AIDN: process.env.NEXT_PUBLIC_AIDN,
+          },
         })
         .then((response) => {
           // status code is between 200-299, we should have the apps
@@ -146,7 +218,5 @@ export default function AdminComponent() {
     getApplications();
   }, [cookies["kreative_id_key"]]);
 
-  return (
-    <div>{apps.length !== 0 && <AdminFunction apps={apps} />}</div>
-  );
+  return <div>{apps.length !== 0 && <AdminFunction apps={apps} />}</div>;
 }
