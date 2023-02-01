70f9486c7a36bf9532f98dc32ff64d76f937296c
Armaan Gupta <armaan@guppy.im>
2023-02-01 11:11:22 -0600

    feat: added configurable error component

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,7 +1,11 @@
 import Head from "next/head";
-import Link from "next/link";
+import ErrorComponent from "@/components/Error";
+import { useRouter } from "next/router";
 
 export default function AdminPage() {
+  const router = useRouter();
+  const { cause } = router.query;
+
   return (
     <>
       <Head>
@@ -14,19 +18,8 @@ export default function AdminPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="pt-12 pb-3 text-center text-2xl">
-          There has been an error while trying to authenticate you...
-        </h1>
-        <p className="text-lg text-gray-400 text-center pb-6">Check the URL parameters for error type</p>
-        <div className="text-center pb-3">
-          <Link href="/admin" className="text-lg text-center text-indigo-500">
-            Try signing in again
-          </Link>
-        </div>
-        <div className="text-center">
-          <Link href="https://support.kreativeusa.com/id" className="text-md text-center text-indigo-400">
-            Contact Kreative Support
-          </Link>
+        <div className="h-full">
+          {cause && <ErrorComponent cause={cause} />}
         </div>
       </main>
     </>
