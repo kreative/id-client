9a092643608a8f7cc0461c8f80fa15cac711febf
Armaan Gupta <armaan@guppy.im>
2023-02-02 11:48:09 -0600

    feat: installed and setup react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,50 +2,56 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CookiesProvider } from "react-cookie";
 import localFont from "@next/font/local";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 // custom craftwork sans fonts loading locally
 const craftworkSans = localFont({
   src: [
     {
-      path: '../public/fonts/CraftworkSans-Regular.woff',
-      weight: '400',
-      style: 'normal',
+      path: "../public/fonts/CraftworkSans-Regular.woff",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../public/fonts/CraftworkSans-Regular.woff2',
-      weight: '400',
-      style: 'normal',
+      path: "../public/fonts/CraftworkSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../public/fonts/CraftworkSans-Regular.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "../public/fonts/CraftworkSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../public/fonts/CraftworkSans-Bold.woff',
-      weight: '700',
-      style: 'normal',
+      path: "../public/fonts/CraftworkSans-Bold.woff",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../public/fonts/CraftworkSans-Bold.woff2',
-      weight: '700',
-      style: 'normal',
+      path: "../public/fonts/CraftworkSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../public/fonts/CraftworkSans-Bold.ttf',
-      weight: '700',
-      style: 'normal',
+      path: "../public/fonts/CraftworkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
   ],
-  variable: '--font-craftworksans',
+  variable: "--font-craftworksans",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CookiesProvider>
-      <div className={`${craftworkSans.variable} font-sans`}>
-        <Component {...pageProps} />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className={`${craftworkSans.variable} font-sans`}>
+          <Component {...pageProps} />
+        </div>
+      </QueryClientProvider>
     </CookiesProvider>
   );
 }
