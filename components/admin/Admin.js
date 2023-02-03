3beeca27736e43ac40cb18713cd506191b29580d
Armaan Gupta <armaan@guppy.im>
2023-02-03 09:56:42 -0600

    feat: created apps table component

--- components/admin/Admin.js
+++ components/admin/Admin.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { useCookies } from "react-cookie";
 
+import AppsTableComponent from "./AppsTable";
 import CreateApplicationModal from "@/components/modals/CreateApplication";
 import AdminNavbarComponent from "@/components/admin/AdminNavbar";
 
-function AdminFunction({ apps }) {
+export default function AdminComponent() {
   // state for create application modal
   const [modalState, setModalState] = useState(false);
 
@@ -33,110 +32,8 @@ function AdminFunction({ apps }) {
             </button>
           </div>
         </div>
-        <div className="mt-8 flex flex-col">
-          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6"
-                      >
-                        AIDN
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
-                      >
-                        Callback URL
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative py-3 pl-3 pr-4 sm:pr-6"
-                      >
-                        <span className="sr-only">Edit</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
-                    {apps.map((app) => (
-                      <tr key={app.aidn}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                          {app.aidn}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {app.name}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {app.callbackUrl}
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                          <a
-                            href="#"
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit
-                            <span className="sr-only">, {app.name}</span>
-                          </a>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
+        <AppsTableComponent />
       </div>
     </div>
   );
-}
-
-export default function AdminComponent() {
-  const [apps, setApps] = useState([]);
-  const [cookies, setCookie, removeCookie] = useCookies(["kreative_id_key"]);
-
-  React.useEffect(() => {
-    let mounted = true;
-
-    // ensures nothing can happen unless the cookie loads
-    if (!cookies["kreative_id_key"]) return;
-
-    const getApplications = () => {
-      // with this axios request, we need to set the proper headers for the server-side authentication
-      // this includes: kreative_id_key, kreative_aidn
-      console.log(cookies["kreative_id_key"]);
-      axios
-        .get("https://id-api.kreativeusa.com/v1/applications", {
-          headers: {
-            KREATIVE_ID_KEY: cookies["kreative_id_key"],
-            KREATIVE_AIDN: process.env.NEXT_PUBLIC_AIDN,
-          },
-        })
-        .then((response) => {
-          // status code is between 200-299, we should have the apps
-          const applications = response.data.data;
-          console.log(applications);
-          setApps(applications);
-          return () => (mounted = false);
-        })
-        .catch((error) => {
-          // some sort of error, statusCode is not between 200-199
-          console.log(error);
-          window.location.href = "/admin/error";
-        });
-    };
-
-    getApplications();
-  }, [cookies["kreative_id_key"]]);
-
-  return <div>{apps.length !== 0 && <AdminFunction apps={apps} />}</div>;
 }
---
+++ components/admin/AppsTable.js
@@ -0,0 +1,116 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useCookies } from "react-cookie";
+
+export default function AppsTableComponent() {
+  const [cookies] = useCookies(["kreative_id_key"]);
+
+  const applicationsQuery = useQuery({
+    queryKey: ["apps"],
+    queryFn: async () => {
+      // create empty response variable
+      let response;
+
+      try {
+        // with this axios request, we need to set the proper headers for the server-side authentication
+        // this includes: kreative_id_key, kreative_aidn
+        response = await axios.get(
+          "https://id-api.kreativeusa.com/v1/applications",
+          {
+            headers: {
+              KREATIVE_ID_KEY: cookies["kreative_id_key"],
+              KREATIVE_AIDN: process.env.NEXT_PUBLIC_AIDN,
+            },
+          }
+        );
+      } catch (error) {
+        // some sort of error, statusCode is not between 200-199
+        console.log(error);
+        // window.location.href = "/admin/error";
+      }
+
+      const applications = response.data.data;
+      return applications;
+    },
+  });
+
+  if (applicationsQuery.isLoading) {
+    return (
+      <h1 className="pt-6 text-center text-lg text-gray-700">
+        Applications are loading...
+      </h1>
+    );
+  }
+
+  if (applicationsQuery.isError) {
+    return (
+      <h1 className="pt-6 text-center text-lg text-gray-700">
+        Error. Applications can't load.
+      </h1>
+    );
+  }
+
+  if (applicationsQuery.isSuccess) {
+    return (
+      <div className="mt-8 flex flex-col">
+        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6"
+                    >
+                      AIDN
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                    >
+                      Callback URL
+                    </th>
+                    <th scope="col" className="relative py-3 pl-3 pr-4 sm:pr-6">
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {applicationsQuery.data.map((app) => (
+                    <tr key={app.aidn}>
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                        {app.aidn}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {app.name}
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {app.callbackUrl}
+                      </td>
+                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Edit
+                          <span className="sr-only">, {app.name}</span>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
