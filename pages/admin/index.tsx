a1570133bfc6b8bc25474c476086d45386621170
Armaan Gupta <armaan@guppy.im>
2023-02-10 23:44:31 -0600

    feat: redirects /admin to /admin/apps for default

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,30 +1,20 @@
 import Head from "next/head";
+import { useEffect } from "react";
 
-import Authenticate from "../../components/Authenticate";
-import AdminComponent from "../../components/admin/Admin";
+export default function AdminHome(): JSX.Element {
 
-// any of the permissions that would allow the user to continue using the application
-const adminPermissions = ["KREATIVE_ID_ADMIN", "KREATIVE_ID_DEVELOPER"];
+  useEffect(() => {
+    window.location.href = "/admin/apps";
+  }, []);
 
-// we should be getting user data in the NextRequest object
-// this would come from the middleware that is authenticating user information
-// the Next middleware should take care of any and all authentication, this means that in this file
-// we can just go ahead and serve the user what they need
-export default function AdminPage() {
   return (
-    <Authenticate permissions={adminPermissions}>
+    <div>
       <Head>
         <title>Admin | Kreative ID</title>
-        <meta
-          name="description"
-          content="First-class authentication for Kreative."
-        />
+        <meta name="description" content="First-class authentication for Kreative." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <AdminComponent />
-      </main>
-    </Authenticate>
-  );
+    </div>
+  )
 }
