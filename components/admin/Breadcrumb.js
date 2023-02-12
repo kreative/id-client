1f3f73755a4d634e2df1cdd2858dac78bd5443f9
Armaan Gupta <armaan@guppy.im>
2023-02-12 13:35:36 -0600

    feat: created variable breadcrumb

---
+++ components/admin/Breadcrumb.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { HomeIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+export default function Breadcrumb({ pages }) {
+  return (
+    <nav className="flex pt-8 pb-8" aria-label="Breadcrumb">
+      <ol role="list" className="flex items-center space-x-4">
+        <li>
+          <div>
+            <Link href="/admin" className="text-gray-400 hover:text-gray-500">
+              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+              <span className="sr-only">Home</span>
+            </Link>
+          </div>
+        </li>
+        {pages.map((page) => (
+          <li key={page.name}>
+            <div className="flex items-center">
+              <ChevronRightIcon
+                className="h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+              <Link
+                href={page.href}
+                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                aria-current={page.current ? "page" : undefined}
+              >
+                {page.name}
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
