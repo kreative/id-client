efb1ce930164a2598e07f10e90e9e3a60987b672
Armaan Gupta <armaan@guppy.im>
2023-02-19 23:14:08 -0600

    feat: added more context and app logo to error

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Image from "next/image";
 import ErrorComponent from "@/components/Error";
 import { useRouter } from "next/router";
 
 export default function AdminPage() {
   const router = useRouter();
-  const { cause } = router.query;
+  const { cause, aidn } = router.query;
 
   return (
-    <>
+    <div className="h-screen">
       <Head>
         <title>Error | Kreative ID</title>
         <meta
@@ -17,11 +18,28 @@ export default function AdminPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="h-full">
-          {cause && <ErrorComponent cause={cause} />}
+      <main className="h-5/6 w-full">
+        <div >
+          {cause && aidn && <ErrorComponent cause={cause} aidn={aidn} />}
         </div>
       </main>
-    </>
+      <footer className="border-t border-gray-100 py-6 sm:py-10">
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-6 sm:flex-row lg:px-8">
+          <p className="text-sm leading-7 text-gray-400">
+            &copy; 2023 Kreative, LLC. All rights reserved.
+          </p>
+          <div className="hidden sm:block sm:h-7 sm:w-px sm:flex-none sm:bg-gray-200" />
+          <div className="flex gap-x-4">
+            <Image
+              className="h-6 w-auto"
+              height={50}
+              width={100}
+              src="https://cdn.kreativeusa.com/id/kreative-id-gray.png"
+              alt="Kreative ID logo in gray"
+            />
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
